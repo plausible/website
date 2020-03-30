a072a137d659cad69dae2b8eb7995d9e9ddbd518
Uku Taht <uku.taht@gmail.com>
2020-03-30 15:32:16 +0300

    Use correct purgecss conf

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   content: ["./_site/**/*.html"],
-  css: ["./_site/css/site.css"],
+  css: ["./_site/assets/css/style.css"],
   defaultExtractor: content => content.match(/[\w-/.:]+(?<!:)/g) || []
 };
