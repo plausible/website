eed91bab1e351807286d3548a1c6ca0dd6b90f2b
Uku Taht <uku.taht@gmail.com>
2022-01-13 19:31:53 -0600

    Sudo mv

--- remove-html.sh
+++ remove-html.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for f in `find /github/workspace/_site -type f -name "*.html"`; do
-    mv -- "$f" "${f%.*}"
+for f in `find _site -type f -name "*.html"`; do
+    sudo mv -- "$f" "${f%.*}"
 done
 
-mv /github/workspace/_site/index /github/workspace/_site/index.html
-mv /github/workspace/_site/admin/index /github/workspace/_site/admin/index.html
+sudo mv _site/index _site/index.html
+sudo mv _site/admin/index _site/admin/index.html
